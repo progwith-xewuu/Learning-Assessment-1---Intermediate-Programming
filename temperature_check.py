06a99612a84bcd5bcf420ff32e9708fa94eb3520
Zylk Mandolado <allenph19@gmail.com>
2026-02-13 23:10:11 +0800

    Delete temperature_check.py

--- temperature_check.py
+++
@@ -1,8 +0,0 @@
-temp = int(input("Current Temperature: "))
-
-if temp > 30:
-    print("Hot Day! Stay Hyrdated. ")
-else:
-    print("Pleasant Weather!")
-if temp < 10:
-    print("It's too Cold!") 
