7901c617494aa4590bfecf133400d284a5f6db93
Zylk Mandolado <allenph19@gmail.com>
2026-02-20 23:14:57 +0800

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,41 +1,83 @@
-user_score = int(input("score here (0-100): "))
-if user_score < 0 or user_score > 100:
-    print("Invalid Scores: Enter a value between 0 and 100.")
+while True:
 
-elif user_score <= 100 and user_score >= 99:
-    grade = "A+"
-    message = "Exceptional"
-elif user_score <= 98 and user_score >= 96:
-    grade = "A"
-    message = "Outstanding"
-elif user_score <= 95 and user_score >= 93:
-    grade = "A-"
-    message = "Excellent"
-elif user_score <= 92 and user_score >= 90:
-    grade = "B+"
-    message = "Very Good"
-elif user_score <= 89 and user_score >= 87:
-    grade = "B"
-    message = "Good"
-elif user_score <= 86 and user_score >= 84:
-    grade = "B-"
-    message = "Solid"
-elif user_score <= 83 and user_score >= 81:
-    grade = "C+"
-    message = "Solid"
-elif user_score <= 80 and user_score >= 78:
-    grade = "C"
-    message = "Passed"
-elif user_score <= 77 and user_score >= 70:
-    grade = "C-"
-    message = "Passed"
-elif user_score <= 69:
-    grade = "F"
-    message = "Failed"
-else:
-    print()
-# print("Score:",user_score, "Grade:",grade, "-",message)
-print(f"Score: {user_score} Grade: {grade} - {message}")
+    user_score = int(input("score here (0-100): "))
+    if user_score < 0 or user_score > 100:
+        print("Invalid Scores: Enter a value between 0 and 100.")
+
+    elif user_score <= 100 and user_score >= 99:
+        grade = "A+"
+        message = "Exceptional"
+    elif user_score <= 98 and user_score >= 96:
+        grade = "A"
+        message = "Outstanding"
+    elif user_score <= 95 and user_score >= 93:
+        grade = "A-"
+        message = "Excellent"
+    elif user_score <= 92 and user_score >= 90:
+        grade = "B+"
+        message = "Very Good"
+    elif user_score <= 89 and user_score >= 87:
+        grade = "B"
+        message = "Good"
+    elif user_score <= 86 and user_score >= 84:
+        grade = "B-"
+        message = "Solid"
+    elif user_score <= 83 and user_score >= 81:
+        grade = "C+"
+        message = "Solid"
+    elif user_score <= 80 and user_score >= 78:
+        grade = "C"
+        message = "Passed"
+    elif user_score <= 77 and user_score >= 70:
+        grade = "C-"
+        message = "Passed"
+    elif user_score <= 69:
+        grade = "F"
+        message = "Failed"
+    else:
+        print()
+
+    print(f"Score: {user_score} Grade: {grade} - {message}")
+
+
+# user_score = float(input("score here (0-100): "))
+# if user_score < 0 or user_score > 100:
+#     print("Invalid Scores: Enter a value between 0 and 100.")
+
+# elif user_score <= 100 and user_score >= 99:
+#     grade = "A+"
+#     message = "Exceptional"
+# elif user_score <= 98 and user_score >= 96:
+#     grade = "A"
+#     message = "Outstanding"
+# elif user_score <= 95 and user_score >= 93:
+#     grade = "A-"
+#     message = "Excellent"
+# elif user_score <= 92 and user_score >= 90:
+#     grade = "B+"
+#     message = "Very Good"
+# elif user_score <= 89 and user_score >= 87:
+#     grade = "B"
+#     message = "Good"
+# elif user_score <= 86 and user_score >= 84:
+#     grade = "B-"
+#     message = "Solid"
+# elif user_score <= 83 and user_score >= 81:
+#     grade = "C+"
+#     message = "Solid"
+# elif user_score <= 80 and user_score >= 78:
+#     grade = "C"
+#     message = "Passed"
+# elif user_score <= 77 and user_score >= 70:
+#     grade = "C-"
+#     message = "Passed"
+# elif user_score <= 69:
+#     grade = "F"
+#     message = "Failed"
+# else:
+#     print()
+# # print("Score:",user_score, "Grade:",grade, "-",message)
+# print(f"Score: {user_score} Grade: {grade} - {message}")
 
 #grade base system
 #https://share.google/0fGMKYaOgIC2HAtDB
