d3e83977e32763a8a10714262e7ae643fba9fca3
Zylk Mandolado <allenph19@gmail.com>
2026-02-20 23:43:05 +0800

    Update movie_ticket_system.py

--- movie_ticket_system.py
+++ movie_ticket_system.py
@@ -1,82 +1,62 @@
-# print("=== MOVIE TICKET SYSTEM ===")
-# day = input("Day: ").lower()
-# customer_type = input("Customer: ").lower()
-# movie_time = int(input("Show time: "))
-# num_ticket = int(input("Number of Tickets : "))
+while True:
 
-# if day == "weekday" or day == "weekend":
-#     if customer_type == "regular" or customer_type == "student" or customer_type == "senior":
-#         if 9 <= movie_time or movie_time <= 22:
-#             if num_ticket > 0:
-#                 if day == "weekday":
-#                     baseprice = 200
-#                 else:
-#                     baseprice = 300
-#                     subtotal = baseprice * num_ticket
-#                     discount = 0
-                
-#                 if customer_type == "student":
-#                     discount = subtotal * 0.20 #20% discount
-#                     percent = "20%"  
-#                 else: 
-#                     customer_type == "senior"
-#                     discount = subtotal * 0.30 #30% discount
-#                     percent = "30%"
-                    
-                
-#                     if num_ticket > 5:
-#                         ticket_discount = num_ticket * 0.05
-#                         percent = "5%"
+    print("\n=== Movie Ticket System ===")
 
-#                 discount += subtotal * 0.10
-                
-#                 final_price = subtotal - discount
+    day = input("Day(weekday/weekend): ").lower().strip()
+    customer = input("Customer Type(Regular/Student/Senior): ").lower().strip()
+    show_time = int(input("Time(hour in 24-hour format): "))
+    num_ticket = int(input("Number of Tickets: "))
 
-#                 print("\n=== RECEIPT ===")
-#                 print(f"Base price: {baseprice:.2f} x {num_ticket} = {subtotal}")
-#                 print(f"{customer_type} discount ({percent}): -{discount} ")
+    print("\n--- Receipt ---")
 
-print("\n=== Movie Ticket System ===")
+    if day == "weekday":
+        price = 200.00 * num_ticket
+        print(f"Base Price: 200.00 x {num_ticket} = {price:.2f}")
+    else: 
+        day == "weekend"
+        price = 300.00 * num_ticket
+        print(f"Base Price: 300.00 x {num_ticket:.2f} = {price}")
 
-day = input("Enter Day: ").lower()
-customer = input("Enter Customer Type: ").lower()
-show_time = int(input("Enter Time: "))
-num_ticket = int(input("Enter Number of Tickets: "))
+        if customer == "regular":
+            discount = 0
+            discounted_price = price * discount
+            price = price - discounted_price
+            print(f"Regular: {discounted_price:.2f}")
 
-print("\n--- Receipt ---")
+        if customer == "student":
+            discount = 0.20
+            discounted_price = price * discount
+            price = price - discounted_price
+            print(f"Student Discount (20%): {discounted_price:.2f}")
 
-if day == "weekday":
-    price = 200.00 * num_ticket
-    print(f"Base Price: 200.00 x {num_ticket} = {price}")
-else: 
-    day == "weekend"
-    price = 300.00 * num_ticket
-    print(f"Base Price: 300.00 x {num_ticket} = {price}")
+        
+        if customer == "senior":
+            discount = 0.30
+            discounted_price = price * discount
+            price = price - discounted_price
+            print(f"Senior Discount (30%): {discounted_price:.2f}")
 
-    if customer == "student":
-        discount = 0.20
+    if show_time < 12:
+        discount = 0.10
         discounted_price = price * discount
         price = price - discounted_price
-        print(f"Student Discount (20%): {discounted_price}")
+        print(f"Matinee Discount (10%): {discounted_price:.2f}")
 
-    else:
-        customer == "senior"
-        discount = 0.30
+    if  num_ticket >= 5:
+        discount = 0.05
         discounted_price = price * discount
         price = price - discounted_price
-        print(f"Senior Discount (30%): {discounted_price}")
+        print(f"Group Discount (5%): {discounted_price:.2f}")
 
-if show_time < 12:
-    discount = 0.10
-    discounted_price = price * discount
-    price = price - discounted_price
-    print(f"Matinee Discount (10%): {discounted_price}")
+    print(f"\nTOTAL: {price:.2f}")
+    print("Thank you for your purchese!")
+    print("\n-------------------------------")
 
-if  num_ticket >= 5:
-    discount = 0.05
-    discounted_price = price * discount
-    price = price - discounted_price
-    print(f"Group Discount (5%): {discounted_price}")
+    again = input("\nDo you want to make another transaction? (yes/no): ").lower().strip()
+    if again != "yes".lower().strip():
+        print("Thank you for your purchese! Come Again!")
+        break
 
 print(f"\nTOTAL: {price:.2f}")
+
 print("Thank you for your purchese!")
